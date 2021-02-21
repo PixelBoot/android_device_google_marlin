4de4641b43fe83ff03ba40738c3a8a717df9136c
spezi77 <spezi7713@gmx.net>
2021-02-21 08:20:07 +0100

    Revert "marlin: Bring in a basic SEPolicy with contexts"
    This reverts commit d1b4e867a6e9071160bf1c293250d8a7c9b70654.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,7 +126,10 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_ROOT_EXTRA_FOLDERS := firmware firmware/radio persist
 BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
 
-BOARD_SEPOLICY_DIRS += device/google/marlin/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/marlin/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/marlin/sepolicy/verizon
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/google/marlin/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/marlin/sepolicy/private
 
 TARGET_FS_CONFIG_GEN := device/google/marlin/config.fs
 
