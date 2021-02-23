b3d7bff337599514ef705fb7b6884cc1b96d35b7
PixelBoot <bootingpixels@gmail.com>
2021-02-23 17:10:31 +0100

    Revert "marlin: Add more packages to be removed"
    This reverts commit f4a9ff7832fc2b4fd9c3fda6a49e2f84a1197805.

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := WellbeingPrebuilt GoogleFeedback RecorderPrebuilt GoogleFeedback Chrome CalendarGooglePrebuilt TipsPrebuilt WallpapersBReel2019 Photos PixelLiveWallpaperPrebuilt GoogleTTS
+LOCAL_OVERRIDES_PACKAGES := WellbeingPrebuilt GoogleFeedback RecorderPrebuilt GoogleFeedback Chrome CalendarGooglePrebuilt TipsPrebuilt WallpapersBReel2019
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
