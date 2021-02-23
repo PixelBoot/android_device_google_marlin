4402c1831bd59711ac41c6fc5b1bfade9c4d0419
PixelBoot <bootingpixels@gmail.com>
2021-02-23 17:10:39 +0100

    Revert "marlin: Remove large packages to make ROM smaller"
    This reverts commit 7640f517af6137a10fc15622617587465c98d991.

--- device-common.mk
+++ device-common.mk
@@ -142,10 +142,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # Light HAL
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl:64
